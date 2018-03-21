09252cebfc81c5308701047e02330107533039bb
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-03-21 11:45:02 +1100

    prototype commit

---
+++ wps.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2016 PyWPS Project Steering Committee
+# 
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import flask
+
+import pywps
+from pywps import Service, Process, BoundingBoxInput, LiteralOutput
+
+class PixelDrill(Process):
+    def __init__(self):
+        inputs = [BoundingBoxInput('pointin',
+                                   'point in',
+                                   ['epsg:4326'])]
+        outputs = [LiteralOutput('avgout',
+                                 'avg out',
+                                 'float')]
+
+        super(PixelDrill, self).__init__(
+            self._handler,
+            identifier       = 'pixeldrill',
+            version          = '0.1',
+            title            = 'WOfS Pixel Drill',
+            abstract         = 'Does WOfS Pixel Drill',
+            inputs           = inputs,
+            outputs          = outputs,
+            store_supported  = False,
+            status_supported = False)
+
+    def _handler(self, request, response):
+        response.outputs['avgout'].data = 67.4
+        return response
+
+
+
+app = flask.Flask(__name__)
+
+processes = [
+    PixelDrill()
+]
+
+# For the process list on the home page
+
+process_descriptor = {}
+for process in processes:
+    abstract = process.abstract
+    identifier = process.identifier
+    process_descriptor[identifier] = abstract
+
+# This is, how you start PyWPS instance
+service = Service(processes, ['pywps.cfg'])
+
+
+@app.route("/")
+def hello():
+    server_url = pywps.configuration.get_config_value("server", "url")
+    request_url = flask.request.url
+    return flask.render_template('home.html', request_url=request_url,
+                                 server_url=server_url,
+                                 process_descriptor=process_descriptor)
+
+
+@app.route('/wps', methods=['GET', 'POST'])
+def wps():
+
+    return service
+    
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="""Script for starting an example PyWPS
+                       instance with sample processes""",
+        epilog="""Do not use this service in a production environment.
+         It's intended to be running in test environment only!
+        For more documentation, visit http://pywps.org/doc
+        """
+        )
+    parser.add_argument('-d', '--daemon',
+                        action='store_true', help="run in daemon mode")
+    parser.add_argument('-a','--all-addresses',
+                        action='store_true', help="run flask using IPv4 0.0.0.0 (all network interfaces),"  +  
+                            "otherwise bind to 127.0.0.1 (localhost).  This maybe necessary in systems that only run Flask") 
+    args = parser.parse_args()
+    
+    if args.all_addresses:
+        bind_host='0.0.0.0'
+    else:
+        bind_host='127.0.0.1'
+
+    if args.daemon:
+        pid = None
+        try:
+            pid = os.fork()
+        except OSError as e:
+            raise Exception("%s [%d]" % (e.strerror, e.errno))
+
+        if (pid == 0):
+            os.setsid()
+            app.run(threaded=True,host=bind_host)
+        else:
+            os._exit(0)
+    else:
+        app.run(threaded=True,host=bind_host)
