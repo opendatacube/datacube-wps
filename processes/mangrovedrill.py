7a511b6d287bda60a73f14d7031df54e204edf86
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-08-07 20:00:13 +1000

    Fix mangrove cover class

--- processes/mangrovedrill.py
+++ processes/mangrovedrill.py
@@ -17,9 +17,9 @@ from processes.geometrydrill import GeometryDrill
 # Data is a list of Datasets (returned from dc.load and masked if polygons)
 def _processData(datas, **kwargs):
     data = datas[0]
-    woodland = data.where( data == 1 ).count(['x', 'y']).rename(name_dict={'mangrove_cover': 'woodland'})
-    open_forest = data.where( data == 2).count(['x', 'y']).rename(name_dict={'mangrove_cover': 'open_forest'})
-    closed_forest = data.where( data == 3).count(['x', 'y']).rename(name_dict={ "mangrove_cover": 'closed_forest'})
+    woodland = data.where( data == 1 ).count(['x', 'y']).rename(name_dict={'canopy_cover_class': 'woodland'})
+    open_forest = data.where( data == 2).count(['x', 'y']).rename(name_dict={'canopy_cover_class': 'open_forest'})
+    closed_forest = data.where( data == 3).count(['x', 'y']).rename(name_dict={ "canopy_cover_class": 'closed_forest'})
 
     final = xarray.merge([wet, dry, notobservable])
     final = final.to_dataframe().to_csv(header=['Woodland', 'Open Forest', 'Closed Forest'],
@@ -35,12 +35,12 @@ tableStyle = {
             "active": True
         },
         "Open Forest": {
-            "units": "%",
+            "units": "#",
             "chartLineColor": "#5ECC00",
             "active": True
         },
         "Closed Forest": {
-            "units": "%",
+            "units": "#",
             "chartLineColor": "#3B7F00",
             "active": True
         }
