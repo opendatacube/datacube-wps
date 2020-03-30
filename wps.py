be753a4a30d02c0cb94d99afc4260cead9a3fef9
Imam Alam <imam.alam@gmail.com>
2020-03-30 11:37:40 +0000

    mangroves too

--- wps.py
+++ wps.py
@@ -1,26 +1,5 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2016 PyWPS Project Steering Committee
-# 
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import os
 import flask
 import sentry_sdk
@@ -28,8 +7,9 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 
 import pywps
 from pywps import Service
-from processes.fcdrill import FcDrill
-from processes.wofsdrill import WofsDrill
+from processes.fcdrill import FCDrill
+from processes.wofsdrill import WOfSDrill
+from processes.mangrovedrill import MangroveDrill
 
 import logging
 import yaml
@@ -55,8 +35,9 @@ with open('DEA_WPS_config.yaml') as fl:
     config = yaml.load(fl)
 
 processes = [
-    WofsDrill(config['processes']['WOfSDrill']['about'], config['processes']['WOfSDrill']['style']),
-    FcDrill(config['processes']['FCDrill']['about'], config['processes']['FCDrill']['style'])
+    WOfSDrill(config['processes']['WOfSDrill']['about'], config['processes']['WOfSDrill']['style']),
+    FCDrill(config['processes']['FCDrill']['about'], config['processes']['FCDrill']['style']),
+    MangroveDrill(config['processes']['MangroveDrill']['about'], config['processes']['MangroveDrill']['style'])
 ]
 
 # For the process list on the home page
