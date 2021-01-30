790940462e93e9f438fcd278b8aa38985c64a495
Tisham Dhar <whatnickd@gmail.com>
2021-01-30 11:21:12 +1100

    #64 Apply mocks to S3 bucket creation

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,6 +1,8 @@
 from moto import mock_s3
+import boto3
 import altair as alt
 from vega_datasets import data
+import pywps.configuration as config
 
 from datacube_wps.processes import upload_chart_svg_to_S3, upload_chart_html_to_S3
 
@@ -10,10 +12,20 @@ TEST_CHART = chart = (
         .encode(x="Horsepower:Q", y="Miles_per_Gallon:Q", color="Origin:N")
     )
 
+TEST_CFG = "pywps.cfg"
+
 @mock_s3
 def test_s3_svg_chart_upload():
+    config.load_configuration(TEST_CFG)
+    bucket = config.get_config_value("s3", "bucket")
+    client = boto3.client("s3")
+    client.create_bucket(Bucket=bucket)
     upload_chart_svg_to_S3(TEST_CHART, "abcd")
 
 @mock_s3
 def test_s3_html_chart_upload():
+    config.load_configuration(TEST_CFG)
+    bucket = config.get_config_value("s3", "bucket")
+    client = boto3.client("s3")
+    client.create_bucket(Bucket=bucket)
     upload_chart_html_to_S3(TEST_CHART, "abcd")
