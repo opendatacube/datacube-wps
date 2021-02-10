002bddad20bc8469b2103e031a1e0ff25a7d1e75
Imam Alam <imam.alam@gmail.com>
2021-02-10 07:19:19 +0000

    fix pyliny issues (this time for sure)

--- datacube_wps/__init__.py
+++ datacube_wps/__init__.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
 import os
-import logging
 import argparse
 
 import flask
-import sentry_sdk
-from sentry_sdk.integrations.flask import FlaskIntegration
-from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 import yaml
 
@@ -19,55 +15,18 @@ from datacube.virtual import construct
 
 from flask import Flask, request
 
-
-LOG_FORMAT = ('%(asctime)s] [%(levelname)s] file=%(pathname)s line=%(lineno)s '
-              'module=%(module)s function=%(funcName)s %(message)s')
-
-
-def setup_logger():
-    logger = logging.getLogger('PYWPS')
-    handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter(LOG_FORMAT))
-    logger.addHandler(handler)
+from .startup_utils import setup_logger, initialise_prometheus, initialise_prometheus_register, setup_sentry
 
 
 setup_logger()
+setup_sentry()
 
-
-def initialise_prometheus(app, log=None):
-    if os.environ.get("prometheus_multiproc_dir", False):
-        metrics = GunicornInternalPrometheusMetrics(app)
-        if log:
-            log.info("Prometheus metrics enabled")
-        return metrics
-    return None
-
-
-def initialise_prometheus_register(metrics):
-    # Register routes with Prometheus - call after all routes set up.
-    if os.environ.get("prometheus_multiproc_dir", False):
-        metrics.register_default(
-            metrics.summary(
-                'flask_wps_request_full_url', 'Request summary by request url',
-                labels={
-                    'query_request': lambda: request.args.get('request'),
-                    'query_url': lambda: request.full_path
-                }
-            )
-        )
-
-
-if os.environ.get("SENTRY_KEY") and os.environ.get("SENTRY_PROJECT"):
-    sentry_sdk.init(dsn="https://%s@sentry.io/%s" % (os.environ["SENTRY_KEY"], os.environ["SENTRY_PROJECT"]),
-                    integrations=[FlaskIntegration()])
 
 app = flask.Flask(__name__)
 
 app.url_map.strict_slashes = False
 
-
 metrics = initialise_prometheus(app)
-
 
 
 def create_process(process, input, **settings):
@@ -151,5 +110,6 @@ if __name__ == "__main__":
 
 
 # Note: register your default metrics after all routes have been set up.
-# Also note, that Gauge metrics registered as default will track the /metrics endpoint, and this can't be disabled at the moment.
+# Also note, that Gauge metrics registered as default will track the /metrics endpoint,
+# and this can't be disabled at the moment.
 initialise_prometheus_register(metrics)
