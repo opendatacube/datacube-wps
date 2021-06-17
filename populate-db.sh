b70f714a7f1ce7555d22b08c6cf29bc5d70826b6
Ben Lewis <ben.lewis@ga.gov.au>
2021-06-17 16:26:22 +1000

    Fixes for WIT test
    Latest C3 FC product definition, and stac/json (rather than yaml) indexing syntax.

--- populate-db.sh
+++ populate-db.sh
@@ -80,30 +80,30 @@ s3-to-dc --skip-lineage --no-sign-request 's3://dea-public-data/WOfS/WOFLs/v2.1.
 s3-to-dc --skip-lineage --no-sign-request 's3://dea-public-data/WOfS/WOFLs/v2.1.5/combined/x_4/y_-32/2000/12/02/*.yaml' wofs_albers
 s3-to-dc --skip-lineage --no-sign-request 's3://dea-public-data/WOfS/WOFLs/v2.1.5/combined/x_4/y_-32/2000/09/13/*.yaml' wofs_albers
 # WIT test datasets
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/03/09/ga_ls_fc_3_091085_2019-03-09_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/03/25/ga_ls_fc_3_091085_2019-03-25_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/04/02/ga_ls_fc_3_091085_2019-04-02_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/04/10/ga_ls_fc_3_091085_2019-04-10_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/04/18/ga_ls_fc_3_091085_2019-04-18_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/04/26/ga_ls_fc_3_091085_2019-04-26_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/05/04/ga_ls_fc_3_091085_2019-05-04_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/05/12/ga_ls_fc_3_091085_2019-05-12_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/05/20/ga_ls_fc_3_091085_2019-05-20_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/05/28/ga_ls_fc_3_091085_2019-05-28_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/06/05/ga_ls_fc_3_091085_2019-06-05_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/06/13/ga_ls_fc_3_091085_2019-06-13_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/06/21/ga_ls_fc_3_091085_2019-06-21_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/06/29/ga_ls_fc_3_091085_2019-06-29_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/03/08/ga_ls_fc_3_092085_2019-03-08_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/03/17/ga_ls_fc_3_092085_2019-03-17_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/04/02/ga_ls_fc_3_092085_2019-04-02_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/04/09/ga_ls_fc_3_092085_2019-04-09_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/04/18/ga_ls_fc_3_092085_2019-04-18_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/04/25/ga_ls_fc_3_092085_2019-04-25_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/05/04/ga_ls_fc_3_092085_2019-05-04_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/05/11/ga_ls_fc_3_092085_2019-05-11_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/05/20/ga_ls_fc_3_092085_2019-05-20_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/06/05/ga_ls_fc_3_092085_2019-06-05_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/06/12/ga_ls_fc_3_092085_2019-06-12_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/06/21/ga_ls_fc_3_092085_2019-06-21_final.stac-item.json ga_ls_fc_3
-s3-to-dc --skip-lineage --no-sign-request s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/07/07/ga_ls_fc_3_092085_2019-07-07_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/03/09/ga_ls_fc_3_091085_2019-03-09_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/03/25/ga_ls_fc_3_091085_2019-03-25_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/04/02/ga_ls_fc_3_091085_2019-04-02_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/04/10/ga_ls_fc_3_091085_2019-04-10_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/04/18/ga_ls_fc_3_091085_2019-04-18_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/04/26/ga_ls_fc_3_091085_2019-04-26_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/05/04/ga_ls_fc_3_091085_2019-05-04_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/05/12/ga_ls_fc_3_091085_2019-05-12_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/05/20/ga_ls_fc_3_091085_2019-05-20_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/05/28/ga_ls_fc_3_091085_2019-05-28_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/06/05/ga_ls_fc_3_091085_2019-06-05_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/06/13/ga_ls_fc_3_091085_2019-06-13_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/06/21/ga_ls_fc_3_091085_2019-06-21_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/091/085/2019/06/29/ga_ls_fc_3_091085_2019-06-29_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/03/08/ga_ls_fc_3_092085_2019-03-08_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/03/17/ga_ls_fc_3_092085_2019-03-17_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/04/02/ga_ls_fc_3_092085_2019-04-02_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/04/09/ga_ls_fc_3_092085_2019-04-09_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/04/18/ga_ls_fc_3_092085_2019-04-18_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/04/25/ga_ls_fc_3_092085_2019-04-25_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/05/04/ga_ls_fc_3_092085_2019-05-04_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/05/11/ga_ls_fc_3_092085_2019-05-11_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/05/20/ga_ls_fc_3_092085_2019-05-20_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/06/05/ga_ls_fc_3_092085_2019-06-05_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/06/12/ga_ls_fc_3_092085_2019-06-12_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/06/21/ga_ls_fc_3_092085_2019-06-21_final.stac-item.json ga_ls_fc_3
+s3-to-dc --skip-lineage --no-sign-request --stac s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/092/085/2019/07/07/ga_ls_fc_3_092085_2019-07-07_final.stac-item.json ga_ls_fc_3
