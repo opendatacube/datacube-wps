2fd4cf564af15d35a00dda641528751870509e89
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-09-09 00:22:14 +0000

    add pylint directly via github workflow

--- lint-code.sh
+++ lint-code.sh
@@ -4,7 +4,7 @@ set -x
 # Consider replacing pycodestyle with autopep8 or flake8
 # See discussions here : https://github.com/pre-commit/pre-commit-hooks/issues/319
 pycodestyle --max-line-length=120 datacube_wps tests
-pylint -j 2 --reports no datacube_wps --disable=C,R,redefined-builtin,fixme,arguments-differ
+#pylint -j 2 --reports no datacube_wps --disable=C,R,redefined-builtin,fixme,arguments-differ
 # bandit -s B101,B104 -r .
 
 set +x
