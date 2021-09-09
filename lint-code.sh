7cd95777015336ff6e5c1bed5de44aad2c21d933
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-09-09 00:09:55 +0000

    move isort into github action

--- lint-code.sh
+++ lint-code.sh
@@ -1,11 +1,10 @@
 set -eu
 set -x
 
-isort --check --diff .
 # Consider replacing pycodestyle with autopep8 or flake8
 # See discussions here : https://github.com/pre-commit/pre-commit-hooks/issues/319
 pycodestyle --max-line-length=120 datacube_wps tests
 pylint -j 2 --reports no datacube_wps --disable=C,R,redefined-builtin,fixme,arguments-differ
-bandit -s B101,B104 -r .
+# bandit -s B101,B104 -r .
 
 set +x
